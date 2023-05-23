d5c00bd77a53e50be9efc885849b39e9cd021fb7
Bobbi Holding-Brown <rholdingbrown@gmail.com>
2023-05-23 16:15:17 +0100

    update fe package version

--- build.py
+++ build.py
@@ -10,7 +10,7 @@ def build_govuk_assets():
     GOVUK_DIR = "/govuk-frontend"
     GOVUK_URL = (
         "https://github.com/alphagov/govuk-frontend/"
-        "releases/download/v4.0.0/release-v4.0.0.zip"
+        "releases/download/v4.6.0/release-v4.6.0.zip"
     )
     ZIP_FILE = "./govuk_frontend.zip"
     DIST_PATH = DIST_ROOT + GOVUK_DIR
